bbb371913bba0da053245d9c73e7db2235ee119a
Maciej Zubala <maciej.zubala@gmail.com>
2016-12-05 21:12:43 +0100

    Initial commit

---
+++ src/main/java/pl/com/bottega/dpex/document/catalog/DocumentCatalog.java
@@ -0,0 +1,4 @@
+package pl.com.bottega.dpex.document.catalog;
+
+public interface DocumentCatalog {
+}
---
+++ src/main/java/pl/com/bottega/dpex/document/reading/ReadingConfirmator.java
@@ -0,0 +1,5 @@
+package pl.com.bottega.dpex.document.reading;
+
+public interface ReadingConfirmator {
+
+}
